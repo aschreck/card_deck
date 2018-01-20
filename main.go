a83f6fa8437f4a567339b49b4752d3d778509227
Aaron Schreck <schreck.aaron@gmail.com>
2018-01-20 11:26:25 -0700

    finalize deck datatype methods/tests

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.shuffle()
+	cards.print()
 }
